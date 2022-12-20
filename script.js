0037c716a6f2a26d8a1f3de216e6af21ce97c490
saurabhje <work.saurabh@outlook.com>
2022-12-20 07:37:33 +0000

    styled grid

---
+++ script.js
@@ -0,0 +1,31 @@
+let a = prompt("Enter your marks");
+if (a <= 100 && a > 90){
+    alert("You have got A+");
+}
+else if (a <= 90 && a > 80){
+alert("You have got A");
+}
+else if (a <= 80 && a > 70){
+    alert("You have got B+");
+}
+else if (a <= 70 && a > 60){
+    alert("You have got B");
+}
+else if (a <= 60 && a > 50){
+    alert("You have got C+");
+}
+else if (a <= 50 && a > 40){
+    alert("You have got C");
+}
+else if (a <= 40 && a > 30){
+    alert("You have got D+");
+}
+else if (a <= 30 && a > 20){
+    alert("You have got D");
+}
+else if (a <= 20 && a > 10){
+    alert("You have got E");
+}
+else if (a <= 10 && a > 0){
+    alert("You have got F");
+}
